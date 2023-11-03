b304b7186d2b21455b9cd9c0e577420497b3109e
El Pancho <fr.espana@duocuc.cl>
2023-11-03 17:32:43 -0300

    Creacion de usuarios Firebase

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,7 +1,9 @@
 import { Injectable, inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { User } from '../models/user.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { getFirestore, setDoc, doc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,7 @@ import { User } from '../models/user.model';
 export class FirebaseService {
 
   auth = inject(AngularFireAuth);
+  firestore = inject(AngularFirestore);
 
   // ====== AUTENTICACIÓN ===== //
 
@@ -16,5 +19,23 @@ export class FirebaseService {
   signIn(user: User) {
     return signInWithEmailAndPassword(getAuth(), user.email, user.password)
   }
+
+  // === SIGN-UP === //
+  signUp(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+  // === ACTUALIZAR USUARIO === //
+  updateUser(displayName: string) {
+    return updateProfile(getAuth().currentUser, { displayName })
+  }
+
+
+  // === BASE DE DATOS === //
+
+    // == SETEAR UN DOCUMENTO == //
+    setDocument(path: string, data: any) {
+      return setDoc(doc(getFirestore(), path), data);
+    }
 
 }
