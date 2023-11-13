efcf9180d69fb834f854edb14ae024436c2dbfa7
duoc <git.duoc@duoc.cl>
2023-11-13 12:46:14 -0300

    Añadidas los porcentajes de asistencia

--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -9,6 +9,15 @@ import { UtilsService } from 'src/app/services/utils.service';
 })
 export class HomePage implements OnInit {
 
+  asignaturas = [
+    { nombre: 'Gestión de proyectos informáticos', porcentaje: 25 },
+    { nombre: 'Calidad de Software', porcentaje: 90 },
+    { nombre: 'Inglés Elemental', porcentaje: 63 },
+    { nombre: 'Estadística descriptiva', porcentaje: 78 },
+    { nombre: 'Seguridad en sistemas computacionales', porcentaje: 15 },
+    // Añadir más asignaturas si es necesario
+  ];
+
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(UtilsService);
 
@@ -18,6 +27,20 @@ export class HomePage implements OnInit {
   // ===== CERRAR SESIÓN ===== //
   signOut(){
     this.firebaseSvc.signOut();
+  }
+
+  aumentarPorcentaje(asignatura) {
+    if (asignatura.porcentaje < 100) {
+      asignatura.porcentaje += 5;
+      if (asignatura.porcentaje > 100) {
+        asignatura.porcentaje = 100;
+      }
+      // Almacenar mensaje en localStorage
+      const mensaje = "Ubicación: Las Palomas 7839, Hora del dispositivo: " + new Date().toLocaleString();
+      localStorage.setItem('mensajeAsistencia', mensaje);
+
+      // Aquí podrías guardar el porcentaje actual en tu base de datos o almacenamiento.
+    }
   }
 
 }
